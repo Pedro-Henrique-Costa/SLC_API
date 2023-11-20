0d7cc54fa8b0568aebc43936b14f72562438cc63
Pedro-Henrique-Costa <pedrohcosta0209@gmail.com>
2023-11-20 17:45:48 -0300

    feat: created methods post, get, getall, put and delete

--- controllers/carController.js
+++ controllers/carController.js
@@ -4,7 +4,63 @@ const carController = {
     
     create: async(req, res) => {
         try {
+            const car = {
+                locador: req.body.locador,
+                reservista: req.body.reservista,
+                pontoDeRetirada: req.body.pontoDeRetirada,
+                statusCar: req.body.statusCar,
+                placa: req.body.placa,
+                modelo: req.body.modelo,
+                precoDiaria: req.body.precoDiaria,
+                foto: req.body.foto
+            };
+            const response = await CarModel.create(car);
+            res.status(201).json({response, msg: "Carro criado com sucesso!"})
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    getAll: async (req, res) => {
+        try {
+            const cars = await CarModel.find();
+
+            res.json(cars);
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    get: async(req, res) => {
+        try {
+            const id = req.params.id;
+            const car = await CarModel.findById(id);
+            if(!car){
+                res.status(404).json({msg: "Carro não encontrado." })
+                return;
+            }
+            res.json(car);
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    delete: async(req, res) => {
+        try {
+            const id = req.params.id
+            const car = await CarModel.findById(id)
+            if(!car){
+                res.status(404).json({msg: "Carro não encontrado." })
+                return;
+            }
+            const deletedCar = await CarModel.findByIdAndDelete(id)
+            res.status(200).json({deletedCar, msg: "Carro excluido com sucesso!"})
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    update: async(req, res) => {
+        try {
             
+            const id = req.params.id
+
             const car = {
                 locador: req.body.locador,
                 reservista: req.body.reservista,
@@ -16,9 +72,14 @@ const carController = {
                 foto: req.body.foto
             };
 
-            const response = await CarModel.create(car);
+            const updatedCar = await CarModel.findByIdAndUpdate(id, car)
 
-            res.status(201).json({response, msg: "Carro criado com sucesso!"})
+            if(!updatedCar){
+                res.status(404).json({msg: "Carro não encontrado." })
+                return;
+            }
+
+            res.status(200).json({car, msg: "Carro atualizado com sucesso!"})
 
         } catch (error) {
             console.log(error)
--- routes/cars.js
+++ routes/cars.js
@@ -6,4 +6,20 @@ router
     .route("/cars")
     .post((req, res) => carController.create(req, res));
 
+router
+    .route("/cars")
+    .get((req, res) => carController.getAll(req, res));
+
+router
+    .route("/cars/:id")
+    .get((req, res) => carController.get(req, res));
+
+router
+    .route("/cars/:id")
+    .delete((req, res) => carController.delete(req, res));
+
+router
+    .route("/cars/:id")
+    .put((req, res) => carController.update(req, res));
+
 module.exports = router;
